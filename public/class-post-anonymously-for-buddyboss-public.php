8fb73ed946b10f956cdd786171bff38cf700ad01
raftaar1191 <raftaar1191@gmail.com>
2023-08-30 19:20:01 +0530

    Fix notification issue

--- public/class-post-anonymously-for-buddyboss-public.php
+++ public/class-post-anonymously-for-buddyboss-public.php
@@ -64,6 +64,28 @@ class Post_Anonymously_For_BuddyBoss_Public {
 	public function bp_init() {
 
 		/**
+		 * Save the meta into the meta table
+		 */
+		$this->save_meta();
+
+		/**
+		 * Render the activity post
+		 */
+		$this->render_activity();
+
+		/**
+		 * Render the notifications
+		 */
+		$this->render_notifications();
+		
+	}
+
+	/**
+	 * Contain all the function are use to save the meta
+	 */
+	public function save_meta() {
+
+		/**
 		 * Add post meta into the Group Activity Meta
 		 */
 		add_action( 'bp_groups_posted_update', array( $this, 'groups_posted_update' ), 1000, 4 );
@@ -72,8 +94,12 @@ class Post_Anonymously_For_BuddyBoss_Public {
 		 * Add post meta into the Activity Meta
 		 */
 		add_action( 'bp_activity_posted_update', array( $this, 'activity_posted_update' ), 1000, 3 );
+	}
 
-
+	/**
+	 * Contain all the function are use to render activity
+	 */
+	public function render_activity() {
 
 		/**
 		 * Hook to add filter so that the normal user can not see the Post author data
@@ -86,15 +112,120 @@ class Post_Anonymously_For_BuddyBoss_Public {
 		 */
 		add_action( 'bp_after_activity_entry', array( $this, 'after_activity_entry' ), 1000 );
 
-
-
 		/**
 		 * Hook to add filter so that the normal user can not see the Post author data
 		 */
 		add_filter( 'bp_groups_format_activity_action_activity_update', array( $this, 'group_activity_update' ), 1000, 2 );
-		
 	}
 
+	/**
+	 * Contain all the function are use to render activity
+	 */
+	public function render_notifications() {
+
+		/**
+		 * Hook to hide the author name in the notifications
+		 */
+		add_filter( 'bb_groups_single_bb_groups_subscribed_activity_notification', array( $this, 'group_activity_notification' ), 1000, 5 );
+
+
+		add_action( 'notifications_loop_start', array( $this, 'notifications_loop_start' ) );
+
+
+		add_action( 'notifications_loop_end', array( $this, 'notifications_loop_end' ) );
+
+	}
+
+	/**
+	 * Nofitication loop started
+	 */
+	public function notifications_loop_start() {
+
+		/**
+		 * Remove the link from the User notification
+		 * Used for: BuddyBoss Theme Only
+		 */
+		add_filter( 'bp_core_get_user_domain', array( $this, 'notifications_user_domain' ), 100, 4 );
+
+
+		/**
+		 * Remove the img from the User notification
+		 * Used for: BuddyBoss Theme Only
+		 */
+		add_filter( 'bp_core_fetch_avatar_url_check', array( $this, 'notifications_user_avatar_url' ), 100, 2 );
+	}
+
+	/**
+	 * Nofitication loop end
+	 */
+	public function notifications_loop_end() {
+
+		/**
+		 * Remove the link from the User notification
+		 * Used for: BuddyBoss Theme Only
+		 */
+		remove_filter( 'bp_core_get_user_domain', array( $this, 'notifications_user_domain' ), 100 );
+
+
+		/**
+		 * Remove the img from the User notification
+		 * Used for: BuddyBoss Theme Only
+		 */
+		remove_filter( 'bp_core_fetch_avatar_url_check', array( $this, 'notifications_user_avatar_url' ), 100 );
+	}
+
+	/**
+	 * Check if the current notification is the anonymously or not
+	 */
+	public function is_anonymously_notifications( $user_id ) {
+
+		$value =  false;
+
+		$notification     	= buddypress()->notifications->query_loop->notification;
+
+		if( ! empty( $notification ) ) {
+			$component        	= $notification->component_name;
+			$component_action	= $notification->component_action;
+			$activity_user_id 	= $notification->secondary_item_id;
+			
+			if( 
+				$user_id == $activity_user_id 
+				&& 'groups' == $component 
+				&& 'bb_groups_subscribed_activity' == $component_action 
+			) {
+				$activity_id 		= $notification->item_id;
+				if( $this->is_anonymously_activity( $activity_id ) ) {
+					$value =  true;
+				}
+			}
+		}
+
+		return $value;
+	}
+
+	/**
+	 * Change the link into the notification area
+	 */
+	public function notifications_user_domain( $domain, $user_id, $user_nicename, $user_login ) {
+
+		if( $this->is_anonymously_notifications( $user_id ) ) {
+			$domain = '';
+		}
+
+		return $domain;
+	}
+
+	/**
+	 * Change the link into the notification area
+	 */
+	public function notifications_user_avatar_url( $avatar_url, $params ) {
+
+		if( ! empty( $params['item_id'] ) && $this->is_anonymously_notifications( $params['item_id'] ) ) {
+			$avatar_url = '';
+		}
+
+		return $avatar_url;
+	}
 
 	/**
 	 * Check where the activity post author is the login user 
@@ -149,7 +280,7 @@ class Post_Anonymously_For_BuddyBoss_Public {
 	/**
 	 * Check if the activity is the anonymously activity
 	 */
-	function is_anonymously_activity( $activity_id, $activity_user_id ) {
+	function is_anonymously_activity( $activity_id ) {
 
 		$value = false;
 		$user_id = get_current_user_id();
@@ -161,7 +292,34 @@ class Post_Anonymously_For_BuddyBoss_Public {
 		return $value;
 	}
 
+	/**
+	 * Update the activity notification
+	 */
+	function group_activity_notification( $content, $notification, $notification_link, $text, $screen ) {
 
+		/**
+		 * If this is for notifications only
+		 */
+		if( in_array( $screen, array( 'web', 'web_push' ) ) ) {
+			$activity_id = $notification->item_id;
+
+			/**
+			 * Check if the post is a anonymously or not
+			 */
+			if( $this->is_anonymously_activity( $activity_id ) ) {
+
+				$user_fullname = bp_core_get_user_displayname( $notification->secondary_item_id );
+
+				$content['text'] = str_replace( $user_fullname, $this->anonymous_user_label(), $content['text'] );
+			}
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Update the activity header seaction
+	 */
 	function group_activity_update( $action, $activity ) {
 
 		/**
@@ -170,7 +328,7 @@ class Post_Anonymously_For_BuddyBoss_Public {
 		if( 
 			! empty( $activity->id ) 
 			&& ! empty( $activity->user_id ) 
-			&& $this->is_anonymously_activity( $activity->id, $activity->user_id ) 
+			&& $this->is_anonymously_activity( $activity->id ) 
 		) {
 			$user_link = $this->anonymous_user_label();
 			if( 
@@ -201,7 +359,7 @@ class Post_Anonymously_For_BuddyBoss_Public {
 		/**
 		 * Check if the activity is anonymously or not
 		 */
-		if( $this->is_anonymously_activity( bp_get_activity_id(), bp_get_activity_user_id() ) ) {
+		if( $this->is_anonymously_activity( bp_get_activity_id() ) ) {
 
 			/**
 			 * For User Link on the Avatar
@@ -293,7 +451,7 @@ class Post_Anonymously_For_BuddyBoss_Public {
 	 * Remove the User Profile Link
 	 */
 	public function anonymous_user_label() {
-		return __( 'Anonymous member', 'post-anonymously-for-buddyboss' );
+		return __( 'Anonymous Member', 'post-anonymously-for-buddyboss' );
 	}
 
 	/**
