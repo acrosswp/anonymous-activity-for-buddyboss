757e21e9c820334c9ed708692aacfbd0f3e4061d
raftaar1191 <raftaar1191@gmail.com>
2023-09-06 20:52:52 +0530

    Add About us content

--- admin/integration/acrosswp-menu.php
+++ admin/integration/acrosswp-menu.php
@@ -75,8 +75,88 @@ class AcrossWP_Main_Menu {
         );
     }
 
-	public function about_acrosswp() {
-		echo "show text about about_acrosswp";
+	function about_acrosswp() {
+		?>
+		<style>
+			.acrosswp-container {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+				justify-content: center;
+				height: 100vh;
+				background-color: #f7f7f7;
+			}
+	
+			.acrosswp-logo img {
+				max-width: 200px;
+				height: auto;
+			}
+	
+			.acrosswp-content {
+				text-align: center;
+				max-width: 600px;
+				margin-top: 20px;
+				padding: 20px;
+				background-color: #fff;
+				border-radius: 10px;
+				box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+			}
+	
+			h2 {
+				color: #0073e6;
+				font-size: 24px;
+			}
+	
+			h3 {
+				color: #333;
+				font-size: 20px;
+			}
+	
+			ul {
+				list-style-type: disc;
+				padding-left: 20px;
+				text-align: left;
+			}
+	
+			p {
+				font-size: 18px;
+			}
+		</style>
+	
+		<div class="acrosswp-container">
+			<div class="acrosswp-logo">
+				<img src="https://example.com/your-image.jpg" alt="AcrossWP Logo">
+			</div>
+	
+			<div class="acrosswp-content">
+				<h2>At AcrossWP</h2>
+				<p style="text-align: left;">We understand the importance of customizing and creating plugins for WordPress to meet our clients’ unique needs.</p>
+				<p style="text-align: left;">Our team of skilled developers has extensive experience in customizing and creating WordPress plugins that are tailored to our clients’ requirements.</p>
+	
+				<h3>Our Specializations:</h3>
+				<ul>
+					<li><strong>E-commerce Development:</strong> We specialize in developing e-commerce sites using WooCommerce, the most popular and widely used e-commerce platform in the world. Our team is proficient in building custom online stores that are visually appealing, user-friendly, and highly functional.</li>
+	
+					<li><strong>Social Networking:</strong> We also specialize in building social networking sites using AcrossWP. Whether you’re looking to build a community for your brand, an online marketplace, or a social network for a particular niche, our team has the expertise to deliver a social networking site that meets your needs.</li>
+	
+					<li><strong>Learning Management Systems (LMS):</strong> Furthermore, we have extensive experience in integrating Learning Management Systems (LMS) using LearnDash plugins. We believe that e-learning is the future, and we work tirelessly to develop online learning platforms that are engaging, interactive, and easy to use.</li>
+				</ul>
+	
+				<h3>Why Choose Us?</h3>
+				<ul>
+					<li>Experienced and dedicated developers</li>
+					<li>Custom solutions tailored to your unique requirements</li>
+					<li>Stunning and user-friendly website designs</li>
+					<li>Timely project delivery</li>
+					<li>Exceptional customer support</li>
+				</ul>
+	
+				<p>We are committed to delivering high-quality web development services that help our clients achieve their business goals.</p>
+	
+				<p>Contact us today to learn more about how we can help you with customizing and creating plugins for WordPress, building e-commerce sites, developing social networking sites, or integrating LMS using LearnDash plugins.</p>
+			</div>
+		</div>
+		<?php
 	}
 
     /**
