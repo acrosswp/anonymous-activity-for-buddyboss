4a90c9e022e40ca0bbd3ad30147330face93d697
raftaar1191 <raftaar1191@gmail.com>
2023-09-21 23:48:59 +0530

    Add code

--- assets/src/frontend/js/index.js
+++ assets/src/frontend/js/index.js
@@ -1,38 +1,18 @@
-// Define your custom Backbone view for the checkbox
-var CustomCheckboxView = Backbone.View.extend({
-  tagName: 'input',
-  attributes: {
-    type: 'checkbox',
-    name: 'anonymously-post',
-    value: '1'
-  },
+// Define your custom Backbone view for the checkbox// Extend bp.Views.FormSubmit
+bp.Views.FormSubmit = bp.Views.FormSubmit.extend({
+  initialize: function() {
+      // Call the original initialize function
+      bp.Views.FormSubmit.__super__.initialize.apply(this, arguments);
 
-  initialize: function(options) {
-    // Additional initialization logic if needed
+      // Create and append the checkbox
+      this.checkbox = new bp.Views.ActivityInput({
+          type: 'checkbox',
+          id: 'anonymously-post',
+          className: 'anonymously-post',
+          name: 'anonymously-post',
+          value: '1'
+      });
+
+      this.views.set( [ this.submit, this.reset, this.discard, this.checkbox ] );
   }
 });
-
-// Assuming you have a view for the activity post form
-var ActivityPostFormView = Backbone.View.extend({
-  initialize: function(options) {
-    // Additional initialization logic if needed
-  },
-  render: function() {
-    // Render the checkbox within the activity post form
-    var customCheckboxView = new CustomCheckboxView();
-
-    // Create label element for the text
-    var label = document.createElement('label');
-    label.textContent = ' Anonymous post';
-
-    this.$el.append(customCheckboxView.el);
-    this.$el.append(label);
-    return this;
-  }
-});
-
-// Create an instance of the ActivityPostFormView and render it
-var activityPostFormView = new ActivityPostFormView({
-  el: '#whats-new-form' // Replace with the appropriate selector
-});
-activityPostFormView.render();
