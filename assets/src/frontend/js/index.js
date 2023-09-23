216e766d30a3663be476c42e8fd526c1e1c19da8
raftaar1191 <raftaar1191@gmail.com>
2023-09-23 20:50:28 +0530

    Add code

--- assets/src/frontend/js/index.js
+++ assets/src/frontend/js/index.js
@@ -13,6 +13,22 @@ bp.Views.FormSubmit = bp.Views.FormSubmit.extend({
           value: '1'
       });
 
+      var checkboxWrapper = document.createElement('div');
+      checkboxWrapper.classList.add('anonymously-post-wrap'); 
+      
+      checkboxWrapper.appendChild(this.checkbox.el);
+      
+      var checkboxknobs = document.createElement('div');
+      checkboxknobs.classList.add('anonymously-post-knobs'); 
+      checkboxWrapper.appendChild(checkboxknobs);
+
+      var checkboxlayer = document.createElement('div');
+      checkboxlayer.classList.add('anonymously-post-layer'); 
+      checkboxWrapper.appendChild(checkboxlayer);
+
+
+      this.checkbox.el = checkboxWrapper;
+
       this.views.set( [ this.submit, this.reset, this.discard, this.checkbox ] );
   }
 });
